6000fe18fd293a9abda44d596a0260035f91de40
Jacob Gersztyn <jabocg@gmail.com>
2019-02-20 00:11:15 -0700

    Add initial database stuff

--- hitherto.py
+++ hitherto.py
@@ -6,17 +6,22 @@ import random
 import sqlite3
 
 
-@contextmanager
-def get_db():
-    connection = sqlite3.connect(args.database)
-    cursor = connection.cursor()
-    yield cursor
-    connection.commit()
-    connection.close()
-
-
 GREETING_STRINGS = ["Hi!", "Hello!", "Hey!", "Wassup?",
                     "Ayyyy :point_right: :sunglasses: :point_right:"]
+COUNT_Q = ("SELECT COUNT(*) "
+           "FROM `servers` "
+           "WHERE `id` = :server_id")
+CREATE_SERVERS = ("CREATE TABLE IF NOT EXISTS `servers` "
+                  "(`id` INTEGER, `name` TEXT, `prefix` TEXT, "
+                  "  `channel_id` INTEGER)")
+CREATE_DAYS = ("CREATE TABLE IF NOT EXISTS `days_since` "
+               "(`server_id` INTEGER, `category` INTEGER, `days` INTEGER)")
+ADD_SERVER = ("INSERT INTO `servers` "
+              "(`id`, `name`, `prefix`, `channel_id`)"
+              "VALUES (?, ?, '+', 0)")
+ADD_DAYS = ("INSERT INTO `days_since` "
+            "(`server_id`, `category`, `days`) "
+            "VALUES (?, ?, 0)")
 
 
 # parse arguments and config
@@ -28,6 +33,17 @@ arg_parser.add_argument('-d', '--database', help='database file',
 args = arg_parser.parse_args()
 cfg_parser = ConfigParser()
 cfg_parser.read(args.identity_file)
+
+
+@contextmanager
+def get_db():
+    # TODO: maybe refactor to use cache
+    connection = sqlite3.connect(args.database)
+    cursor = connection.cursor()
+    yield cursor
+    connection.commit()
+    connection.close()
+
 
 # create client
 client = discord.Client()
@@ -56,9 +72,15 @@ async def on_message(message):
             await report_days(message)
 
 
-async def report_days(message, category=None):
-    server_id = message.server.id
-    channel = message.channel
+async def reset_days(server, ban=False):
+    """Reset the days since kick/ban."""
+    if ban:
+        pass
+    else:
+        pass
+
+
+async def report_days(server, channel, category=None):
     if category == 'kick':
         days = 0
     elif category == 'ban':
@@ -67,15 +89,36 @@ async def report_days(message, category=None):
         category = 'kick/ban'
         days = 2
 
-    msg = ('It has been {days} since the last {category}'
+    msg = ('It has been {days} day(s) since the last {category}.'
            .format(days=days, category=category))
     await client.send_message(channel, msg)
 
 
+def init_db():
+    with get_db() as db:
+        db.execute(CREATE_SERVERS)
+        db.execute(CREATE_DAYS)
+
+
+def in_db(server):
+    with get_db() as db:
+        server_count = db.execute(COUNT_Q, (server.id,)).fetchone()[0]
+    return server_count > 0
+
+
+def add_to_db(server):
+    with get_db() as db:
+        db.execute(ADD_SERVER, (server.id, server.name,))
+        db.executemany(ADD_DAYS, ((server.id, 0,), (server.id, 1,),))
+
+
 @client.event
 async def on_ready():
-    pass
-
+    init_db()
+    for s in client.servers:
+        if not in_db(s):
+            add_to_db(s)
+    print('ready')
 
 # start client
 print('starting client...')
